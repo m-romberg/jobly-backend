f14ba489f807be8b2bf29732949e46b2467a4003
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-28 14:32:07 -0700

    initial jobly backend

---
+++ middleware/auth.js
@@ -0,0 +1,79 @@
+"use strict";
+
+/** Convenience middleware to handle common auth cases in routes. */
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError } = require("../expressError");
+
+
+/** Middleware: Authenticate user.
+ *
+ * If a token was provided, verify it, and, if valid, store the token payload
+ * on res.locals (this will include the username and isAdmin field.)
+ *
+ * It's not an error if no token was provided or if the token is not valid.
+ */
+
+function authenticateJWT(req, res, next) {
+  const authHeader = req.headers?.authorization;
+  if (authHeader) {
+    const token = authHeader.replace(/^[Bb]earer /, "").trim();
+
+    try {
+      res.locals.user = jwt.verify(token, SECRET_KEY);
+    } catch (err) {
+      /* ignore invalid tokens (but don't store user!) */
+    }
+  }
+  return next();
+
+}
+
+/** Middleware to use when they must be logged in.
+ *
+ * If not, raises Unauthorized.
+ */
+
+function ensureLoggedIn(req, res, next) {
+  if (res.locals.user?.username) return next();
+  throw new UnauthorizedError();
+}
+
+
+/** Middleware to use when they be logged in as an admin user.
+ *
+ *  If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+  if (res.locals.user?.username && res.locals.user?.isAdmin === true) {
+    return next();
+  }
+  throw new UnauthorizedError();
+
+}
+
+/** Middleware to use when they must provide a valid token & be user matching
+ *  username provided as route param.
+ *
+ *  If not, raises Unauthorized.
+ */
+
+function ensureCorrectUserOrAdmin(req, res, next) {
+  const user = res.locals.user;
+  const username = res.locals.user?.username;
+  if (username && (username === req.params.username || user.isAdmin === true)) {
+    return next();
+  }
+
+  throw new UnauthorizedError();
+}
+
+
+module.exports = {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUserOrAdmin,
+};
---
+++ routes/users.js
@@ -0,0 +1,125 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+const { BadRequestError } = require("../expressError");
+const User = require("../models/user");
+const { createToken } = require("../helpers/tokens");
+const userNewSchema = require("../schemas/userNew.json");
+const userUpdateSchema = require("../schemas/userUpdate.json");
+
+const router = express.Router();
+
+
+/** POST / { user }  => { user, token }
+ *
+ * Adds a new user. This is not the registration endpoint --- instead, this is
+ * only for admin users to add new users. The new user being added can be an
+ * admin.
+ *
+ * This returns the newly created user and an authentication token for them:
+ *  {user: { username, firstName, lastName, email, isAdmin }, token }
+ *
+ * Authorization required: admin
+ **/
+
+router.post("/", ensureAdmin, async function (req, res, next) {
+  const validator = jsonschema.validate(
+      req.body,
+      userNewSchema,
+      { required: true },
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const user = await User.register(req.body);
+  const token = createToken(user);
+  return res.status(201).json({ user, token });
+});
+
+
+/** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
+ *
+ * Returns list of all users.
+ *
+ * Authorization required: admin
+ **/
+
+router.get("/", ensureAdmin, async function (req, res, next) {
+  const users = await User.findAll();
+  return res.json({ users });
+});
+
+
+/** GET /[username] => { user }
+ *
+ * Returns { username, firstName, lastName, isAdmin, jobs }
+ *   where jobs is { id, title, companyHandle, companyName, state }
+ *
+ * Authorization required: admin or same user-as-:username
+ **/
+
+router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  const user = await User.get(req.params.username);
+  return res.json({ user });
+});
+
+
+/** PATCH /[username] { user } => { user }
+ *
+ * Data can include:
+ *   { firstName, lastName, password, email }
+ *
+ * Returns { username, firstName, lastName, email, isAdmin }
+ *
+ * Authorization required: admin or same-user-as-:username
+ **/
+
+router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  const validator = jsonschema.validate(
+      req.body,
+      userUpdateSchema,
+      { required: true },
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const user = await User.update(req.params.username, req.body);
+  return res.json({ user });
+});
+
+
+/** DELETE /[username]  =>  { deleted: username }
+ *
+ * Authorization required: admin or same-user-as-:username
+ **/
+
+router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  await User.remove(req.params.username);
+  return res.json({ deleted: req.params.username });
+});
+
+
+/** POST /[username]/jobs/[id]  { state } => { application }
+ *
+ * Returns {"applied": jobId}
+ *
+ * Authorization required: admin or same-user-as-:username
+ * */
+
+router.post("/:username/jobs/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
+  const jobId = +req.params.id;
+  await User.applyToJob(req.params.username, jobId);
+  return res.json({ applied: jobId });
+});
+
+
+module.exports = router;
